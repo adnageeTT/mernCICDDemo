3b95a0822b24539a27398558709121e5205f5cff
adnageeTT <adnagee@gmail.com>
2025-04-23 16:14:24 -0400

    ttesting dev branch

--- backend/server.js
+++ backend/server.js
@@ -42,4 +42,4 @@ app.post('/register', async(req, res) => {
 
 
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`Server running on part ${PORT}`));
+app.listen(PORT, () => console.log(`Server running on part in dev ${PORT}`));
