fd167dc6484b13e78195f23df1e2b279da4b4462
adnageeTT <adnagee@gmail.com>
2025-04-30 14:51:24 -0400

    added error msg

--- backend/server.js
+++ backend/server.js
@@ -35,7 +35,7 @@ app.post('/register', async(req, res) => {
         await newUser.save();
         res.status(201).json({ message: 'User registered successfully!' });
     } catch (err) {
-        res.status(500).json({ error: 'Registration failed' });
+        res.status(500).json({ error: 'Registration failed' + err.message});
     }
 });
 
